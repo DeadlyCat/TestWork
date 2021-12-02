ded38fa02e47a5f8502cc4d136f737b01967a259
DeadlyCat <77786426+DeadlyCat@users.noreply.github.com>
2021-12-03 01:52:06 +0300

    Update FindTextEvents.cs

--- Work/Assets/Scripts/FindTextEvents.cs
+++ Work/Assets/Scripts/FindTextEvents.cs
@@ -3,7 +3,7 @@ using UnityEngine.Events;
 public class FindTextEvents : MonoBehaviour
 {
     [SerializeField] private UnityEvent _start;
-    void Start()
+    private void Start()
     {
         _start.Invoke();
     }
